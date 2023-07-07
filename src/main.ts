2d99a81dff9c0ebbfa5754c47d3b8cea797fd098
Konrad <k.marek@cez.gov.pl>
2023-07-07 17:16:39 +0200

    add validation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { CreateProductDto } from './product/dto/create-product.dto';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
   .setTitle('Test api example')
