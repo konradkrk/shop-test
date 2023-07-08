94e15c59c8f46974b60d63f0e086770e46d2c862
Konrad <k.marek@cez.gov.pl>
2023-07-08 08:09:03 +0200

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   controllers: [UsersController],
 })
